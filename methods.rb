369b3b86d52cdec5bf2887d9c4e595b388f685d8
Idris Noor <idreesnoor150@gmail.com>
2023-05-16 14:54:41 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+
+  def greet(name)
+    puts "hello, #{Naureen}!"
+  
+
+    def greet_with_default(name = "programmer")
+        puts "hello, #{Sunny}!"
+
+        def add(number1, number2)
+            number1 + number2
+          end
+
+          
+          
+        def halve(number)
+            return number / 2 if number.is_a?(Numeric)
+            nil
+          end
+          
